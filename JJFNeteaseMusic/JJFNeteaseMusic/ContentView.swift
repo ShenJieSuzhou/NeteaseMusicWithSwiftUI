a2d8ee61cc6be199842addc4fcfb57503f5038e4
ShenJieSuzhou <745689122@qq.com>
2020-08-14 16:23:14 +0800

    初始化工程
    初始化工程

---
+++ JJFNeteaseMusic/JJFNeteaseMusic/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  JJFNeteaseMusic
+//
+//  Created by shenjie on 2020/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JJFNeteaseMusic/JJFNeteaseMusic/JJFNeteaseMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJFNeteaseMusicApp.swift
+//  JJFNeteaseMusic
+//
+//  Created by shenjie on 2020/8/14.
+//
+
+import SwiftUI
+
+@main
+struct JJFNeteaseMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
